c13abd272ca8d1aa4ed39fef48b3fa262ee1c94d
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-12 21:22:57 +0530

    test-version-6-components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Comment from './Comment';
 import * as serviceWorker from './serviceWorker';
 
 // Simple Component - javascript functions
@@ -37,32 +38,6 @@ function AppTest() {
 /* ReactDOM.render( <AppTest /> , document.getElementById('component')); */
 //Here we can't call like given below because its not a variable declared with const
 //Its a function.
-
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-function Comment(props){
-  return(
-      <div className = 'Comment'>
-        <div className = 'UserInfo'>
-            <img 
-                className = "Avatar"
-                src = {props.author.avatarUrl}
-                alt = {props.author.name}
-            />
-            <div className = 'UserInfo-name'>
-                {props.author.name}
-            </div>
-        </div>
-        <div className = 'Comment-text'>
-            {props.text}
-        </div>
-        <div className = 'Comment-date'>
-            {formatDate(props.date)} 
-        </div>
-      </div>
-  );
-}
 
 const comment = {
   text : 'I hope you enjoy learning React!',
