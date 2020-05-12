eb688c82f237e27a2af4d351fd99d8b9bae6aa9a
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-12 21:11:05 +0530

    test-version-5-components

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+// Simple Component - javascript functions
 function tick() {
   const element = (
     <div>
@@ -18,9 +12,9 @@ function tick() {
       <h2>It is {new Date().toLocaleTimeString()}</h2>
     </div>
   )
-  ReactDOM.render(element, document.getElementById('root'))
+  ReactDOM.render(element, document.getElementById('component'))
 }
-setInterval(tick, 100);
+setInterval(tick, 10000); //calling the tick() in setInterval()
 
 // components in react
 function Welcome(props){
@@ -28,7 +22,8 @@ function Welcome(props){
 }
 
 const welc = <Welcome name = 'Sara' />
-ReactDOM.render(welc, document.getElementById('component'));
+/* ReactDOM.render(welc, document.getElementById('component')); */
+
 function AppTest() {
   return(
     <div>
@@ -39,9 +34,57 @@ function AppTest() {
   );
 }
 
-ReactDOM.render( <AppTest /> , document.getElementById('component'));
+/* ReactDOM.render( <AppTest /> , document.getElementById('component')); */
 //Here we can't call like given below because its not a variable declared with const
 //Its a function.
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+function Comment(props){
+  return(
+      <div className = 'Comment'>
+        <div className = 'UserInfo'>
+            <img 
+                className = "Avatar"
+                src = {props.author.avatarUrl}
+                alt = {props.author.name}
+            />
+            <div className = 'UserInfo-name'>
+                {props.author.name}
+            </div>
+        </div>
+        <div className = 'Comment-text'>
+            {props.text}
+        </div>
+        <div className = 'Comment-date'>
+            {formatDate(props.date)} 
+        </div>
+      </div>
+  );
+}
+
+const comment = {
+  text : 'I hope you enjoy learning React!',
+  date : new Date(),
+  author : {
+      name : 'Hello Kitty',
+      avatarUrl : 'https://placekitten.com/g/64/64',
+  },
+};
+
+//calling the App.js file
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> {welc} <AppTest /> <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  </React.StrictMode>, 
+  document.getElementById('root')
+);
+
 //ReactDOM.render( AppTest, document.getElementById('component'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
