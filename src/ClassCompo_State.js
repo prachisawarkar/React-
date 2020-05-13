a0fd82437547c162d264b8a2a32332b8decf21e9
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-13 21:14:08 +0530

    test-version-9-extract-components

--- src/ClassCompo_State.js
+++ src/ClassCompo_State.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(props) {
-    return(
-        <div>
-            <h3>Hello, World! This is from State and Lifecycle.</h3>
-            <h4>It is {props.date.toLocaleTimeString()}.</h4>
-        </div>
-    );
+/*
+Five steps to convert function component into class compoennt
+
+1. Create an ES6 class, with the same name, that extends React.Component.
+2. Add a single empty method to it called render().
+3. Move the body of the function into the render() method.
+4. Replace props with this.props in the render() body.
+5. Delete the remaining empty function declaration.
+
+*/
+class Clock extends React.Component { 
+    render() {
+        return(
+            <div>
+                <h3>Hello, World! This is from State and Lifecycle.</h3>
+                <h4>It is {this.props.date.toLocaleTimeString()}.</h4>
+            </div>
+        );
+    }
 }
 
 function ticktick() {
