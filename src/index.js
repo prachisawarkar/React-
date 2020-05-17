351f7f073883b0fad4ae241db2b823035e5ccdc0
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-17 15:56:14 +0530

    test-version-16-conditional_rendering

--- src/index.js
+++ src/index.js
@@ -1,64 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Comment from './Comment'; 
-import ticktick from './ClassCompo_State';
-import Toggle from './eventHandling';
+import LoginControl from './conditionalRendering';
 import * as serviceWorker from './serviceWorker';
 
-// Simple Component - javascript functions
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, World!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  )
-  ReactDOM.render(element, document.getElementById('component'))
-}
-setInterval(tick, 10000); //calling the tick() in setInterval()
-
-// components in react
-function Welcome(props){
-  return <h1>Hello, {props.name}</h1>
-}
-
-const welc = <Welcome name = 'Sara' />
-/* ReactDOM.render(welc, document.getElementById('component')); */
-
-function AppTest() {
-  return(
-    <div>
-      <Welcome name = 'Sara' />
-      <Welcome name = 'Cahal' />
-      <Welcome name = 'Tana' />
-    </div>
-  );
-}
-
-/* ReactDOM.render( <AppTest /> , document.getElementById('component')); */
-//Here we can't call like given below because its not a variable declared with const
-//Its a function.
-
-const comment = {
-  text : 'I hope you enjoy learning React!',
-  date : new Date(),
-  author : {
-      name : 'Hello Kitty',
-      avatarUrl : 'https://placekitten.com/g/64/64',
-  },
-};
-
-//calling the App.js file and all other files as well
 ReactDOM.render(
-  <React.StrictMode>
-    <App /> {welc} <AppTest /> <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />
-  </React.StrictMode>, 
+  <LoginControl />,
   document.getElementById('root')
 );
 
