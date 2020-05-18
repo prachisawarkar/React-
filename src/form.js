16d5d5751e37b63e358996ec21114b512ef1bc2e
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-18 19:06:56 +0530

    test-version-24-form-select

--- src/form.js
+++ src/form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class NameForm extends React.Component {
+/*class NameForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -32,6 +32,43 @@ class NameForm extends React.Component {
             </form>
         );
     }
+}*/
+
+class SelectForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected_value :  'coconut'
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            selected_value : event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        alert('Your favourite flavor is : ' + this.state.selected_value);
+        event.preventDefault();
+    }
+    render() {
+        return (
+            <form onSubmit = {this.handleSubmit}>
+                <label> Pick your favorite flavor : <br /> <br />
+                    <select value = {this.state.selected_value} onChange = {this.handleChange} >
+                        <option value = 'grapefruit'>Grapefruit</option>
+                        <option value = 'lime'>Lime</option>
+                        <option value = 'coconut'>Coconut</option>
+                        <option value = 'mango'>Mango</option>
+                    </select>
+                </label>
+                <input type = 'submit' value = 'Submit' />
+            </form>
+        );
+    }
 }
 
-export default NameForm;
+export default SelectForm;
